d20b9f8a80d5e3a3db8a8834019dde4876438c2a
suppakorn.rakna <68814521+ibzzsfw@users.noreply.github.com>
2021-12-29 16:28:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 
     PARAMETER = {
         'knapsack': knapsack,
-        'generation': 2000,
+        'generation': 2000, #customizable
         'genome_size': knapsack.n,
-        'population_size': int((np.pi**2+np.pi)*np.sqrt(knapsack.n)),
+        'population_size': int((np.pi**2+np.pi)*np.sqrt(knapsack.n)), #customizable
         'crossover_method': 'UNIFORM', # CROSSOVER METHOD: SINGLE_POINT, TWO_POINT, UNIFORM
-        'crossover_rate': 0.8,
+        'crossover_rate': 0.8, #customizable
         'mutation_method': 'BIT_FLIP', # MUTATION METHOD:  BIT_FLIP, SWAP
-        'mutation_rate': 0.1,
+        'mutation_rate': 0.1, #customizable
     }
 
     GA(**PARAMETER).natural_selection()
